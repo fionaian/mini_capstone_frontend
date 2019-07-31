bf5b643a3e9e7b43d16e39fb9bdd1e3cf26e63b0
Fiona Ian <sayhitofiona@gmail.com>
2019-07-30 19:32:06 -0500

    test, signup, login, logout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,125 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>New Product</h1>
+    Name:
+    <input v-model="name" type="text" />
+    Supplier:
+    <input v-model="supplier" type="text" />
+    Price:
+    <input v-model="price" type="text" />
+    Description:
+    <input v-model="description" type="text" />
+    Image URL:
+    <input v-model="imageUrl" type="text" />
+    <button v-on:click="createProduct()">Create</button>
+    <h1>{{ message }}</h1>
+    <div v-for="product in products">
+      <h2>{{ product.name }}</h2>
+      <img v-bind:src="product.image_url" alt="" />
+      <div>
+        <button v-on:click="showProduct(product)">More info</button>
+      </div>
+      <div v-if="product === currentProduct">
+        <p>Price: {{ product.price }}</p>
+        <p>Description: {{ product.description }}</p>
+        <h4>Edit product</h4>
+        <div>
+          Name:
+          <input v-model="product.name" type="text" />
+          Price:
+          <input v-model="product.price" type="text" />
+          Description:
+          <input v-model="product.description" type="text" />
+          <button v-on:click="updateProduct(product)">Update Product</button>
+        </div>
+        <button v-on:click="destroyProduct(product)">Delete Product</button>
+      </div>
+    </div>
   </div>
 </template>
 
+<style>
+img {
+  width: 100%;
+}
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "THE S N A C K SHOP!",
+      products: [],
+      currentProduct: {},
+      name: "",
+      supplier: "",
+      price: "",
+      description: "",
+      imageUrl: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/products").then(response => {
+      this.products = response.data;
+      console.log(this.products);
+    });
+  },
+  methods: {
+    createProduct: function() {
+      console.log("Create the product...");
+      var params = {
+        name: this.name,
+        price: this.price,
+        description: this.description,
+        supplier_id: 1,
+        image_url: this.imageUrl
+      };
+      axios
+        .post("/api/products", params)
+        .then(response => {
+          console.log("Success", response.data);
+          this.products.push(response.data);
+          this.name = "";
+          this.supplier = "";
+          this.price = "";
+          this.description = "";
+          this.imageUrl = "";
+        })
+        .catch(error => console.log(error.response));
+    },
+    showProduct: function(inputProduct) {
+      if (this.currentProduct === inputProduct) {
+        this.currentProduct = {};
+      } else {
+        this.currentProduct = inputProduct;
+      }
+    },
+    updateProduct: function(inputProduct) {
+      var params = {
+        name: inputProduct.name,
+        supplier: inputProduct.supplier,
+        price: inputProduct.price,
+        description: inputProduct.description,
+        image_url: inputProduct.imageUrl
+      };
+      axios.patch("/api/products/" + inputProduct.id, params).then(response => {
+        console.log("Update successful", response.data);
+        inputProduct.name = response.data.name;
+        inputProduct.supplier = response.data.supplier;
+        inputProduct.price = response.data.price;
+        inputProduct.description = response.data.description;
+        inputProduct.imageUrl = response.data.imageUrl;
+      });
+    },
+    destroyProduct: function(inputProduct) {
+      axios.delete("/api/products/" + inputProduct.id).then(response => {
+        console.log("Delete successful", response.data);
+        var index = this.products.indexOf(inputProduct);
+        this.products.splice(index, 1);
+      });
+    }
   }
-}
+};
 </script>
